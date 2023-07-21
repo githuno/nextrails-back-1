18c1f79c1adef6015555279adddbd8f37ceba2a9
githuno <clas.warehouse@gmail.com>
2023-07-21 11:19:53 +0900

    環境変数の呼び出し方法を変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,11 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+# 環境変数を呼び出す
+port = ENV['PORT1']
+# ポート番号を切り出す
+hostp = port.split(':')[1] || port
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "localhost:#{hostp}"
