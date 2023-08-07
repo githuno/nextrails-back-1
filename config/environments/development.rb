634b112699c9f20422a0ecf1a1140c212522721b
githuno <clas.warehouse@gmail.com>
2023-08-08 01:57:27 +0900

    development.rb修正（403エラー対応）

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,6 +66,6 @@ Rails.application.configure do
   # localhost:3000では通信に失敗するため
   # hostをdocker-compose.ymlのコンテナ名に合わせる
   config.hosts << "backend"
-  config.hosts << /[a-zA-Z0-9-]+\.github\.dev/ # codespace専用
+  config.hosts << /[a-zA-Z0-9-]+\.app\.github\.dev/ # codespace専用(ここが間違っていると403エラーになる)
 
 end
